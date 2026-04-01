ae39b053b8007dfd620565638d3a115f3e50281e
noahrose82 <73205797+noahrose82@users.noreply.github.com>
2026-03-31 19:41:58 -0700

    Remove local and sensitive files

--- backend/dist/test-db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const db_1 = require("./db");
-async function test() {
-    const [rows] = await db_1.pool.query('SELECT 1 + 1 AS result');
-    console.log(rows);
-    process.exit(0);
-}
-test();
